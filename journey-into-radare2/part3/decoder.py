01ce59ed6faeda4ae25f1152be082568df70f02e
Charles Ellis <hamled@hamled.name>
2019-06-11 17:55:09 -0700

    Part 3: Improve decoder script

--- journey-into-radare2/part3/decoder.py
+++ journey-into-radare2/part3/decoder.py
@@ -2,15 +2,15 @@
 import sys
 
 
-DECODE_TABLE = "AaCcdDeFfGhiKLlMmnNoOpPrRsSTtUuVvwWxyZz32.\\EbgjHI _YQB:\"/@\r\n\x1A"
+DEFAULT_TABLE = "AaCcdDeFfGhiKLlMmnNoOpPrRsSTtUuVvwWxyZz32.\\EbgjHI _YQB:\"/@\r\n\x1A"
 
-def decode(str):
-    indexes = bytes(str, 'ascii')
-    return ''.join([ DECODE_TABLE[x] for x in indexes[::2] ])
+def decode(indexes, table=DEFAULT_TABLE):
+    return ''.join([ table[x] for x in indexes[::2] ])
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        decoded = decode(sys.argv[1])
+        input = sys.argv[1]
     else:
-        decoded = decode(sys.stdin.readline())
-    print(decoded)
+        input = sys.stdin.readline()
+
+    print(decode(bytes(input, 'ascii')))
